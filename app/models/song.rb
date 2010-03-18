5c512baf9bcfed371ea0b447bf09e9d7f5430d5c
unixcharles <unixcharles@gmail.com>
2010-03-18 19:01:17 -0400

    refactoring

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,10 +4,8 @@ class Song < ActiveRecord::Base
   
   named_scope :published, :conditions => {:published => true}, :order => :position
   
+  belongs_to :resource
+  
   validates_presence_of :title
   validates_uniqueness_of :title
-  
-  def file
-    Resource.find(resource_id).public_filename if resource_id
-  end
 end
