8ae648170c65566696ca68efd30bf3f4c9310d8c
Charles Barbier <unixcharles@gmail.com>
2010-05-21 21:37:12 -0700

    admin section is now at /refinery

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :songs, :as => 'music'
 
-  map.namespace(:admin) do |admin|
+  map.namespace(:admin, :path_prefix => 'refinery') do |admin|
     admin.resources :songs, :as => 'music'
     admin.resources :music_settings
   end
