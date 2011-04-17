296b07adec0b83082654761a68698b86a7324dc0
Charles Barbier <unixcharles@gmail.com>
2011-04-17 14:55:46 -0700

    Fix and cleanup setting controller a bit

--- app/controllers/admin/music_settings_controller.rb
+++ app/controllers/admin/music_settings_controller.rb
@@ -1,22 +1,14 @@
 module Admin
   class MusicSettingsController < Admin::BaseController
-
     crudify :music_setting, :title_attribute => :name, :order => "id ASC", :searchable => false
 
-
-    def toggle_setting
+    def toggle
       @music_setting = MusicSetting.find(params[:id])
       @music_setting.toggle!(:value)
       
-      flash[:notice] = "Comment from '#{@music_setting.name}' has been set to " +
-      if @music_setting.value
-        "true"
-      else
-        "false"
-      end
+      flash[:notice] = "Comment from '#{@music_setting.name}' has been set to '#{@music_setting.value ? 'true' : 'false'}'"
 
       redirect_to :action => 'index'
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Refinery::Application.routes.draw do
   resources :music, :as => :songs, :controller => :songs
   #resources :songs, :as => :music
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-    resources :music_settings
+    resources :music_settings do
+      member do
+        match :toggle
+      end
+    end
     resources :music, :as => :songs, :controller => :songs, :except => :show do
       collection do
         post :update_positions
